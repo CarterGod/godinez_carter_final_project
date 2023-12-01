268d00fa3d95473d05295187610e89ae0742398f
CarterGod <cartergodinez@gmail.com>
2023-12-01 12:57:01 -0800

    test push

---
+++ settings.py
@@ -0,0 +1,18 @@
+# This file was created by Carter Godinez
+
+# game settings 
+WIDTH = 720
+HEIGHT = 720
+FPS = 30
+
+# player settings
+PLAYER_JUMP = 30
+PLAYER_GRAV = 2.5
+PLAYER_FRIC = 0.2
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
